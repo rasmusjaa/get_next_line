65e3874c53986eac07abfc593160b6fdca678739
Rasmus Jaakonmaki <rjaakonm@c2r4p10.hive.fi>
2019-10-21 18:56:18 +0300

    prototypes and header to be edited

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rjaakonm <rjaakonm@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/21 16:00:36 by rjaakonm          #+#    #+#             */
+/*   Updated: 2019/10/21 16:21:50 by rjaakonm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+
+int	get_next_line(const int fd, char **line);
+
+#endif
